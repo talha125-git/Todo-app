6d7549e04ff18dbbddf642cb7e399c1d26a3dcf2
talha125-git <malikabutalharaheem@gamil.com>
2026-03-18 08:54:22 -0700

    debug token

--- frontend/src/componants/Login.jsx
+++ frontend/src/componants/Login.jsx
@@ -22,9 +22,12 @@ const Login = () => {
       result = await result.json()
       console.log("Login response:", result);
       if (result.success) {
-        localStorage.setItem('authToken', result.token)  // ✅ add this
+        localStorage.setItem('authToken', result.token)
         localStorage.setItem('login', userData.email)
         localStorage.setItem('userName', result.userName)
+
+        alert("Token saved: " + localStorage.getItem('authToken')) // ✅ add this temporarily
+
         window.dispatchEvent(new Event('localStorage-change'))
         navigate('/')
       } else {
